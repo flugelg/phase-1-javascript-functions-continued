27789c2bf1bfec5eb8748cac2de38e0ce9c8d289
Gary Flugel <flugelg@gmail.com>
2021-11-29 14:47:50 -0500

    commit tree a7d718909fa148145e089f8b33e8ee38e480640a

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (activity != null) {
+        return `This Saturday, I want to ${activity}!`;
+    }
+
+    else {
+        return `This Saturday, I want to roller-skate!`;
+    }
+}
+
+function mondayWork(work) {
+    if (work != null) {
+        return `This Monday, I will ${work}.`;
+    }
+
+    else {
+        return `This Monday, I will go to the office.`;
+    }
+}
+
+function wrapAdjective(flair) {
+    return function (name) {
+        if (name != null) {
+            return `You are ${flair}${name}${flair}!`;
+        }
+    
+        else {
+            return `You are *special*`;
+        }
+    };
+  }
